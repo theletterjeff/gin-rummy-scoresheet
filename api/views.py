141f7a4774f9f38062dd1892316ee3c8a5354104
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-04-09 09:33:38 -0400

    Add test view returning all Match data

--- api/views.py
+++ api/views.py
@@ -1,7 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from base.models import Game, Match, Outcome, Score
+from .serializers import (GameSerializer, MatchSerializer,
+                          OutcomeSerializer, ScoreSerializer)
+
 @api_view(['GET'])
-def getData(request):
-    person = {'name': 'Jeff', 'age': 34}
-    return Response(person)
+def getAllMatches(request):
+    """
+    Return all Match instances.
+    """
+    matches = Match.objects.all()
+    serializer = MatchSerializer(matches, many=True)
+    
+    return Response(serializer.data)
