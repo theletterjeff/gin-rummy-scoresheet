0b411159ba290050812d94bdceaeff2f76ebf796
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-04-09 09:02:28 -0400

    Add datetime_started test for Match

--- base/tests/test_models.py
+++ base/tests/test_models.py
@@ -1,9 +1,13 @@
 """
 Tests for the models for the base app.
 """
+from datetime import datetime, timezone, timedelta
+from unittest import mock
+
 from django.test import TestCase
 
-from base.models import Match
+from accounts.models import Player
+from base.models import Match, Score
 
 class TestMatchModel(TestCase):
     """
@@ -17,3 +21,14 @@ class TestMatchModel(TestCase):
         match = Match()
         self.assertEqual(match.target_score, 500)
         self.assertFalse(match.complete)
+    
+    @mock.patch('django.utils.timezone.now')
+    def test_create_match_sets_datetime_started_to_now(self, mock_datetime):
+        """
+        Creating a new Match sets the `datetime_started` attribute to now.
+        """
+        tz = timezone(timedelta(hours=-4))
+        mock_datetime.return_value = datetime(2022, 1, 1, tzinfo=tz)
+        
+        match = Match.objects.create()
+        assert match.datetime_started == mock_datetime()
