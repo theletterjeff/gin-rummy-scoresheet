458dcc5791cb9ca38e89c7f4b8d8cda15dae7a44
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-04-09 09:46:58 -0400

    Add names to URLs

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getAllMatches)
+    path('', views.getAllMatches, name='all_matches'),
+    path('create-game/', views.createGame, name='create_game'),
 ]
