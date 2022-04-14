af08062197177bc066a3d2e78b8a369ea4167e5a
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-04-13 20:06:12 -0400

    Add URLS: all matches/games, create match/game

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getAllMatches, name='all_matches'),
+    path('all-matches', views.getAllMatches, name='all_matches'),
+    path('all-games', views.getAllGames, name='all-games'),
     path('get-match/<str:pk>/', views.getMatch, name='get_match'),
     path('get-game/<str:pk>/', views.getGame, name='get_game'),
+    path('create-match', views.createMatch, name='create_match'),
+    path('create-game', views.createGame, name='create_game'),
 ]
