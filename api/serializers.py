97cd5ff5a212cb0627cced7a9674fffa18a93823
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-04-13 20:05:03 -0400

    Add PlayerSerializer
    For use in MatchSerializer many to many field

--- api/serializers.py
+++ api/serializers.py
@@ -1,23 +1,36 @@
 from rest_framework import serializers
 
+from accounts.models import Player
 from base.models import Game, Match, Outcome, Score
 
+class PlayerSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Player
+        fields = '__all__'
+
 class GameSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Game
         fields = '__all__'
 
 class MatchSerializer(serializers.ModelSerializer):
+
+    players = PlayerSerializer(many=True, read_only=True)
+
     class Meta:
         model = Match
         fields = '__all__'
 
 class OutcomeSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Outcome
         fields = '__all__'
 
 class ScoreSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Score
         fields = '__all__'
