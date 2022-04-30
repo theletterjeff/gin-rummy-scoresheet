f3d915eae4adead857c36d344a46dfa65f5ae920
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-04-30 12:54:15 -0400

    WIP fill games table, get winner usernames

--- frontend/views.py
+++ frontend/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'frontend/home.html')
 
-def match(request):
+def match(request, match_id):
     return render(request, 'frontend/match.html')
