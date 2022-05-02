a8ceb3137ed78f7c7682b4de642d43c492fc35ac
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-02 07:21:09 -0400

    Add public documentation (include_docs_urls)

--- grs_project/settings.py
+++ grs_project/settings.py
@@ -113,6 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 
