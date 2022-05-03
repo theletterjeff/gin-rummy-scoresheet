97a0de82501bb7099a45a9652dbe34b0e790b982
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-03 17:36:50 -0400

    Delete home view

--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'frontend/home.html')
-
 def match(request, match_id):
     return render(request, 'frontend/match.html')
