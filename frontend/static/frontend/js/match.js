6c1e6c60115458a6eeb3b20dda38f4cb33164b02
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-03 18:28:51 -0400

    Rewrite listGames in Match JS class

--- frontend/static/frontend/js/match.js
+++ frontend/static/frontend/js/match.js
@@ -1,166 +1,76 @@
-fillMatchesNavLink();
-let players = {} 
-listGames();
-fillWinners();
-
 class Match {
 
   constructor() {
-    this.players = {};
-    this.matchEndpoint = this._getMatchEndpoint();
+    this.players = {}
+    this.matchEndpoint = getMatchEndpoint()
+    this.gameWrapper = document.getElementById("game-wrapper")
   }
 
-  _getMatchEndpoint() {
-    const pageURL = new URL(window.location.href);
-    const pageOrigin = pageURL.origin;
-    const pagePath = pageURL.pathname;
-    return `${pageOrigin}/api${pagePath}`;
+  fillPage() {
+    getJSONResponsePromise(this.MatchEndpoint)
+    .then(function(matchData) {
+      let players = getPlayers(matchData);
+      this.players.push(players);
+      return matchData;
+    })
+    .then(function(matchData) {
+      let gameEndpoints = matchData.games;
+      for (let i in gameEndpoints) {
+        getJSONResponsePromise(gameEndpoints[i])
+        .then(function(gameData) {
+          this.gameWrapper += makeCard(gameData, this.players, i)
+        });
+      };
+    });
   }
-
 }
 
-// Get promise of JSON response from API call
+
 function getJSONResponsePromise(endpoint) {
   let json = fetch(endpoint)
-  .then((response) => response.json());
+             .then((response) => response.json);
   return json;
-};
-
-function getPlayers() {
-    let matchEndpoint = getMatchEndoint();
-    let players = [];
-
-    getJSONResponsePromise(matchEndpoint)
-    .then(function(data) {
-        console.log(data.players);
-        for (playerEndpoint of data.players) {
-            fetch(playerEndpoint)
-            .then((resp) => resp.json())
-            .then(function(playerData) {
-                players.push(playerData);
-            });
-        };
-    });
-    return players;
-};
-
-/**
- * Take in an array of Player objects, return an object mapping 
- * endpoint to username. Used to cross-reference the player endpoints 
- * we get back from listGames.
- * @param {Object} players  Player data loaded from API.
- */
-function getPlayerUsernames(players) {
-    let endpointUsername = {};
-    for (let player of players) {
-        endpointUsername[player.url] = player.username;
-    };
-    return endpointUsername;
 }
 
-/**
- * Build a list of games and fill div with data from those games.
- * @param {Object} players   Player data loaded from API.
- */
-function listGames() {
-    
-    let wrapper = document.getElementById('game-wrapper');
-    let matchEndpoint = getMatchEndoint();
-
-    fetch(matchEndpoint)
-    .then((resp) => resp.json())
-    .then((matchJSON) => fillPlayersArray(matchJSON))
-    .then(function(data) {
-        let gameEndpoints = data.games;
-
-        for (let i in gameEndpoints) {
-
-            getJSONResponsePromise(gameEndpoints[i])
-            .then(function(data) {
-                let item = makeCard(data, idx=i);
-                wrapper.innerHTML += item;
-                return data;
-            })
-            .then(() => fillWinners());
-            
-            
-        };
-    
-    });
-    
-    
-};
-
-function makeCard(gameResponseJson, idx) {
-    
-    let winnerEndpoint = gameResponseJson.winner
-    let datePlayed = new Date(gameResponseJson.datetime_played).toDateString()
-    let points = gameResponseJson.points
-    let gin = gameResponseJson.gin
-    let undercut = gameResponseJson.undercut
-
-    let innerHTML = `
-        <div class="card mt-4 mb-4" id="game-card-${idx}">
-            <div class="card-body row">
-                <div class="col">
-                    <h6>Winner</h6>
-                    <h5 class="winner">${winnerEndpoint}<h5>
-                </div>
-                <div class="col">
-                    <h6>Played</h6>
-                    <h5>${datePlayed}</h5>
-                </div>
-                <div class="col">
-                    <h6>Points</h6>
-                    <h5>+${points}</h5>
-                </div>
-            </div>
-        </div>
-    `;
-
-    return innerHTML;
-};
-
-function fillPlayersArray(matchJSON) {
-  for (playerEndpoint of matchJSON.players) {
+function getPlayers(matchData) {
+  players = {}
+  for (playerEndpoint of matchData.players) {
     getJSONResponsePromise(playerEndpoint)
-    .then((playerJSON) => players[playerEndpoint] = playerJSON.username)
-    .then(console.log(players));
-  };
-
-  return matchJSON;
-};
-
-function fillWinners() {
-    let winnerElems = document.getElementsByClassName("winner");
-
-    for (let winnerElem of winnerElems) {
-        fetch(winnerElem.innerHTML)
-        .then((response) => response.json())
-        .then(function(data) {
-            winnerElem.innerHTML = data.username;
-        });
-    };
-
-};
-
-function getUsername(playerEndpoint) {
-    let username = fetch(playerEndpoint)
-    .then((resp) => resp.json())
-    .then(function(data) {
-        return data.username;
+    .then(function(playerData) {
+      players[playerEndoint] = playerData.username;
     });
-    return username;
-};
+  };
+}
 
-function getMatchesURL() {
-    const pageURL = new URL(window.location.href);
-    const pageOrigin = pageURL.origin;
-    return pageOrigin + "/api/matches";
-};
-function fillMatchesNavLink() {
-    const matchesURL = getMatchesURL();
-    matchesNavElem = document.getElementById("matches-nav-link");
-    matchesNavElem.href = matchesNavElem.href.replace(
-        "#", matchesURL);
-};
+function makeCard(gameData, playersObj, idx) {
+  let winnerEndpoint = gameData.winner
+  let winnerUsername = playersObj[winnerEndpoint]
+
+  let datePlayed = new Date(gameData.datetime_played).toDateString()
+  let points = gameData.points
+  let gin = gameData.gin
+  let undercut = gameData.undercut
+
+  let innerHTML = `
+    <div class="card mt-4 mb-4" id="game-card-${idx}">
+      <div class="card-body row">
+        <div class="col">
+          <h6>Winner</h6>
+          <h5>${winnerUsername}</h5>
+        </div>
+        <div class="col">
+          <h6>Played</h6>
+          <h5>${datePlayed}</h5>
+        </div>
+        <div class="col">
+          <h6>Points</h6>
+          <h5>${points}</h5>
+        </div>
+      </div>
+    </div>
+  `
+  return innerHTML;
+}
+
+let match = new Match();
+match.fillPage();
