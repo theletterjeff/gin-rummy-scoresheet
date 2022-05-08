15e53439aa08383f9813bf02806d62fa34a564af
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-08 15:48:21 -0400

    Add default permission and authentication classes

--- grs_project/settings.py
+++ grs_project/settings.py
@@ -112,6 +112,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # API
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema',
