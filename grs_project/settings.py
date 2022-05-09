699240946259f7e3c0c5bf17485f7b867109453b
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-09 08:15:51 -0400

    Remove templates dirs value

--- grs_project/settings.py
+++ grs_project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'grs_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'frontend/templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
