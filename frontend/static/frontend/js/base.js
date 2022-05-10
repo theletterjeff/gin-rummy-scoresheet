705cacafff5e3eae44185688f72b3c1593c54db8
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-10 07:19:48 -0400

    Add fillNavbarBrandLink function

--- frontend/static/frontend/js/base.js
+++ frontend/static/frontend/js/base.js
@@ -1,4 +1,13 @@
+fillNavbarBrandLink();
+
+// Functions
+
 export function fillTitle(titleString) {
   let title = document.getElementsByTagName('title')[0];
   title.innerHTML = titleString;
+}
+
+function fillNavbarBrandLink() {
+  let navbarBrand = document.getElementById("navbar-brand");
+  navbarBrand.href = window.location.origin;
 }
