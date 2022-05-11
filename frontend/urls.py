c1c7d2347c5171b95fe6027b601fd83ee0ad7b87
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-11 08:32:46 -0400

    Move auth URLs to frontend urls.py

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
     path('match/<int:match_id>', views.match, name='match-detail'),
+    path('', include('django.contrib.auth.urls')),
 ]
--- grs_project/urls.py
+++ grs_project/urls.py
@@ -7,7 +7,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
