b003f217fb144d6ec17d9b060fd5819dd3b8e2e7
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-12 07:25:05 -0400

    Add getAPIEndpointRoot

--- frontend/static/frontend/js/base.js
+++ frontend/static/frontend/js/base.js
@@ -1,13 +1,21 @@
 fillNavbarBrandLink();
 
-// Functions
+// Base template functions
+
+function fillNavbarBrandLink() {
+  let navbarBrand = document.getElementById("navbar-brand");
+  navbarBrand.href = window.location.origin;
+}
+
+// Export functions
 
 export function fillTitle(titleString) {
   let title = document.getElementsByTagName('title')[0];
   title.innerHTML = titleString;
 }
 
-function fillNavbarBrandLink() {
-  let navbarBrand = document.getElementById("navbar-brand");
-  navbarBrand.href = window.location.origin;
+export function getAPIEndpointRoot() {
+    const pageURL = new URL(window.location.href);
+    const pageOrigin = pageURL.origin;
+    return `${pageOrigin}/api`;
 }
