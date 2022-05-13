cd0012e18d201241bbfb87c52f262b220d351ea7
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-13 19:21:09 -0400

    Include django auth URLs in urlpatterns

--- grs_project/urls.py
+++ grs_project/urls.py
@@ -5,11 +5,10 @@ from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
+    path('', include('frontend.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
-
-    # API Documentation ()
     path('api-docs/', include_docs_urls(title='Gin Rummy Scoresheet API')),
-    path('', include('frontend.urls')),
 ]
