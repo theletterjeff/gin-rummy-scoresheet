b95f74785f922fd2e0fb6fe1f744310fab2ddf60
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-13 19:19:21 -0400

    Remove custom login URL and view

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('match/<int:match_id>', views.match, name='match-detail'),
-    path('login/', views.login, name='login'),
 
     # path('', include('django.contrib.auth.urls')),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,8 +3,5 @@ from django.shortcuts import render
 def match(request, match_id):
     return render(request, 'frontend/match.html')
 
-def login(request):
-    return render(request, 'registration/login.html')
-
 def home(request):
     return render(request, 'frontend/home.html')
