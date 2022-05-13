4bba214c275bec4a4b990295b7dbfc8fc996ed8f
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-13 08:04:28 -0400

    WIP: JS login

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,6 +2,9 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('match/<int:match_id>', views.match, name='match-detail'),
-    path('', include('django.contrib.auth.urls')),
+    path('login/', views.login, name='login'),
+
+    # path('', include('django.contrib.auth.urls')),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def match(request, match_id):
     return render(request, 'frontend/match.html')
+
+def login(request):
+    return render(request, 'frontend/login.html')
+
+def home(request):
+    return render(request, 'frontend/home.html')
