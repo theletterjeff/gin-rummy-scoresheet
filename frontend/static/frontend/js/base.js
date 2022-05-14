54775b203eaaf331f360133084651f5dae5374fa
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-14 08:50:15 -0400

    Delete parseJwt function

--- frontend/static/frontend/js/base.js
+++ frontend/static/frontend/js/base.js
@@ -45,13 +45,3 @@ export function getCookie(name) {
   }
   return cookieValue;
 }
-
-export function parseJwt (token) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
-
-  return JSON.parse(jsonPayload);
-};
