41068e33a7edd5fc8fb9d0396232cd7cce41f744
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-15 08:59:41 -0400

    Add / to match URL, create game edit URL

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('match/<int:match_id>', views.match, name='match-detail'),
-
-    # path('', include('django.contrib.auth.urls')),
+    path('match/<int:match_id>/', views.match, name='match-detail'),
+    path('game/<int:game_id>/', views.game_edit, name='game-edit'),
 ]
