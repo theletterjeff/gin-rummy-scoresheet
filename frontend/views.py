12216c8c49521357f9f92b3809e5c3266eb892f5
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-15 08:59:50 -0400

    Add game edit view

--- frontend/views.py
+++ frontend/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 def match(request, match_id):
     return render(request, 'frontend/match.html')
 
+def game_edit(request, game_id):
+    return render(request, 'frontend/game-edit.html')
+
 def home(request):
     return render(request, 'frontend/home.html')
