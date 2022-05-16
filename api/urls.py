b9f991991f8f29a1358efaba6da53bea9e3de9a0
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-05-16 07:39:44 -0400

    Remove JWT endpoints from API URLs

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework_simplejwt.views import (TokenRefreshView, TokenVerifyView)
 from . import views
 
 urlpatterns = [
@@ -21,10 +20,6 @@ urlpatterns = [
     path('game/<str:pk>/', views.GameDetail.as_view(), name='game-detail'),
     path('player/<str:pk>/', views.PlayerDetail.as_view(), name='player-detail'),
 
-    # JWT
-    path('token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
