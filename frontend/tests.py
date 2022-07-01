950cba088947d3334117c91fae71df82d503fb4a
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-07-01 12:10:43 -0400

    Create setup scripts for selenium tests

--- frontend/tests.py
+++ frontend/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
-# Create your tests here.
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.firefox.options import Options
+
+from accounts.models import Player
+
+class FrontEndTests(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Create driver"""
+        super().setUpClass()
+
+        options = Options()
+        options.headless = True
+
+        cls.driver = WebDriver(options=options)
+    
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Close driver"""
+        cls.driver.quit()
+        super().tearDownClass()
+    
+    def setUp(self):
+        """Create and log in user (aka player)"""
+        super(FrontEndTests, self).setUp()
+
+        # Create user (aka player)
+        player = Player.objects.create(username='username')
+        player.set_password('password')
+        player.save()
+
+        # Log in user
+        self.assertTrue(self.client.login(
+            username='username',
+            password='password',
+        ))
+
+        # Add cookie to log in the browser
+        cookie = self.client.cookies['sessionid']
+        self.driver.get(self.live_server_url)
+        self.driver.add_cookie(
+            {
+                'name': 'sessionid',
+                'value': cookie.value,
+                'secure': False,
+                'path': '/',
+            }
+        )
