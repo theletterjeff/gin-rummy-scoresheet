8fe4d845053a88a9ea6b18ae07a94d988ccdf886
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-01 15:49:07 -0400

    Remove extra args and context from frontend views

--- frontend/views.py
+++ frontend/views.py
@@ -3,19 +3,20 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'frontend/home.html')
 
-def player_detail(request, username):
+def player_detail(request):
     return render(request, 'frontend/player-profile.html')
 
-def player_edit(request, username):
+def player_edit(request):
     return render(request, 'frontend/player-edit.html')
 
-def match_list(request, username):
+def match_list(request):
     return render(request, 'frontend/matches.html')
 
-def match_detail(request, username, match_pk):
+def match_detail(request):
     return render(request, 'frontend/match.html')
 
-def game_detail(request, username, match_pk, game_pk)
+def game_detail(request):
+    return render(request, 'frontend.game-detail.html')
 
-def game_edit(request, username, match_pk, game_pk):
+def game_edit(request):
     return render(request, 'frontend/game-edit.html')
