405d5eb6b8dfc78d454c09e621a26070a391544e
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-02 14:30:05 -0400

    WIP rewrite API URLs--player detail, match list & detail

--- api/urls.py
+++ api/urls.py
@@ -7,26 +7,32 @@ urlpatterns = [
     # Root
     path('', views.api_root, name='api-root'),
 
-    # List and Create
+    path('player/<str:username>', views.PlayerDetail.as_view(), name='player-detail'),
+
     path('matches/<str:username>/', views.MatchList.as_view(), name='match-list'),
-    path('game/', views.GameList.as_view(), name='game-list-create'),
-    path('all-players/', views.PlayerList.as_view(), name='player-list'),
+    path('matches/<int:match_pk>/', views.MatchDetail.as_view(), name='match-detail'),
 
-    # Player matches (to refactor/rename later)
-    path('player-matches/', views.PlayerMatches.as_view(), name='player-matches'),
 
-    # Players
-    path('player-create/', views.PlayerCreate.as_view(), name='player-create'),
-    path('player/<str:username>/', views.PlayerDetail.as_view(), name='player-detail'),
-    path('logged-in-player/', views.LoggedInPlayerDetail.as_view(), name='logged-in-player'),
+#     # List and Create
+#     path('matches/<str:username>/', views.MatchList.as_view(), name='match-list'),
+#     path('game/', views.GameList.as_view(), name='game-list-create'),
+#     path('all-players/', views.PlayerList.as_view(), name='player-list'),
 
-    # Detail
-    path('match/<str:pk>/', views.MatchDetail.as_view(), name='match-detail'),
-    path('game/<str:pk>/', views.GameDetail.as_view(), name='game-detail'),
-    path('match/player-score/<str:pk>/', views.ScoreDetail.as_view(),
-         name='score-detail'),
-    path('match/player-outcome/<str:pk>/', views.OutcomeDetail.as_view(),
-         name='outcome-detail'),
+#     # Player matches (to refactor/rename later)
+#     path('player-matches/', views.PlayerMatches.as_view(), name='player-matches'),
+
+#     # Players
+#     path('player-create/', views.PlayerCreate.as_view(), name='player-create'),
+#     path('player/<str:username>/', views.PlayerDetail.as_view(), name='player-detail'),
+#     path('logged-in-player/', views.LoggedInPlayerDetail.as_view(), name='logged-in-player'),
+
+#     # Detail
+#     path('match/<str:pk>/', views.MatchDetail.as_view(), name='match-detail'),
+#     path('game/<str:pk>/', views.GameDetail.as_view(), name='game-detail'),
+#     path('match/player-score/<str:pk>/', views.ScoreDetail.as_view(),
+#          name='score-detail'),
+#     path('match/player-outcome/<str:pk>/', views.OutcomeDetail.as_view(),
+#          name='outcome-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
