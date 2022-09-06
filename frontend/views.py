3e6ae9751e330b3cbe8692f36ff8933dc6f8b506
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-06 11:43:49 -0400

    Add kwargs to views

--- frontend/views.py
+++ frontend/views.py
@@ -6,23 +6,23 @@ def home(request):
 def player_list(request):
     raise NotImplementedError
 
-def player_detail(request):
+def player_detail(request, username: str):
     return render(request, 'frontend/player-detail.html')
 
-def player_edit(request):
+def player_edit(request, username: str):
     return render(request, 'frontend/player-edit.html')
 
-def match_list(request):
+def match_list(request, username: str):
     return render(request, 'frontend/match-list.html')
 
-def match_detail(request):
+def match_detail(request, match_pk: int):
     return render(request, 'frontend/match-detail.html')
 
-def match_edit(request):
+def match_edit(request, match_pk: int):
     return render(request, 'frontend/match-edit.html')
 
-def game_detail(request):
+def game_detail(request, match_pk: int, game_pk: int):
     return render(request, 'frontend.game-detail.html')
 
-def game_edit(request):
+def game_edit(request, match_pk: int, game_pk: int):
     return render(request, 'frontend/game-edit.html')
