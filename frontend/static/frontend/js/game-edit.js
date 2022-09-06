9cddac711c82b92e9b332b22850f516bb93105d3
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-06 16:57:08 -0400

    Get usernames directly from player endpoints, fill winner dropdowns

--- frontend/static/frontend/js/game-edit.js
+++ frontend/static/frontend/js/game-edit.js
@@ -1,31 +1,37 @@
-import { fillTitle, getJsonResponse } from './utils.js';
-import { getApiDetailEndpoint, getFrontendURL } from './endpoints.js';
-import { fillWinnerDropdown, fillPoints, fillCheckbox, submitGameForm } from './game-form.js';
+import { fillTitle, getJsonResponse, getUsernameFromEndOfURL } from './utils.js';
+import { getDetailEndpointFromEditURL,
+         getFrontendURL } from './endpoints.js';
+import { fillPoints, fillCheckbox,
+         submitGameForm } from './game-form.js';
 
-const gameDetailEndpoint = getApiDetailEndpoint();
+const gameDetailEndpoint = getDetailEndpointFromEditURL();
 
 fillGameEditPage();
 
+/**
+ * Main function for filling the Game Edit page.
+ */
 async function fillGameEditPage() {
   fillTitle('Edit Game');
   fillGameEditForm();
 }
 
+/**
+ * Populate form fields with current data, add PATCH event to submit button.
+ */
 async function fillGameEditForm() {
   const gameJson = await getJsonResponse(gameDetailEndpoint);
-  
-  const winnerEndpoint = gameJson.winner;
-  const winnerJson = getJsonResponse(winnerEndpoint);
   
   const matchDetailEndpoint = gameJson.match;
 
   // Values to use in form
-  const winnerUsername = winnerJson.username;
+  const winnerUsername = getUsernameFromEndOfURL(gameJson.winner);
+  const loserUsername = getUsernameFromEndOfURL(gameJson.loser);
   const points = gameJson.points;
   const gin = gameJson.gin;
   const undercut = gameJson.undercut;
 
-  fillWinnerDropdown(matchDetailEndpoint, winnerUsername);
+  fillWinnerDropdown(winnerUsername, loserUsername);
   fillPoints(points);
   fillCheckbox("gin-input", gin);
   fillCheckbox("undercut-input", undercut);
@@ -40,4 +46,16 @@ async function addGameEditSubmitEvent(matchDetailEndpoint) {
     .then((resp) => console.log(resp.status))
     .then(() => {window.location = getFrontendURL(matchDetailEndpoint)})
   })
+}
+
+/**
+ * Fill the 'winner' dropdown with players' usernames.
+ */
+function fillWinnerDropdown(winnerUsername, loserUsername) {
+  let dropdownOptions = ```
+    <option select value=${winnerUsername}>${winnerUsername}</option>
+    <option values=${loserUsername}>${loserUsername}</option>
+  ```
+  let winnerDropdown = document.getElementById('winner-dropdown');
+  winnerDropdown.innerHTML = dropdownOptions;
 }
