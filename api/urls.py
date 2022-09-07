4fe7d41cd37bc53249f6d8443f58643941dc104c
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-07 09:54:00 -0400

    Add line end pattern match for API urls
    URLs without variables didn't have them before, causing mismatches

--- api/urls.py
+++ api/urls.py
@@ -3,14 +3,14 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    re_path(r'^players/', views.PlayerList.as_view(), name='player-list'),
+    re_path(r'^players/$', views.PlayerList.as_view(), name='player-list'),
     re_path(r'^players/(?P<username>[a-zA-z]+.*)/$', views.PlayerDetail.as_view(), name='player-detail'),
-    re_path(r'^players/create/', views.PlayerCreate.as_view(), name='player-create'),
-    re_path(r'^logged-in-player/', views.LoggedInPlayerDetail.as_view(), name='logged-in-player'),
+    re_path(r'^players/create/$', views.PlayerCreate.as_view(), name='player-create'),
+    re_path(r'^logged-in-player/$', views.LoggedInPlayerDetail.as_view(), name='logged-in-player'),
 
     re_path(r'^matches/(?P<username>[a-zA-z]+.*)/$', views.MatchList.as_view(), name='match-list'),
     re_path(r'^matches/(?P<match_pk>[0-9]+)/$', views.MatchDetail.as_view(), name='match-detail'),
-    re_path(r'^matches/create/', views.MatchCreate.as_view(), name='match-create'),
+    re_path(r'^matches/create/$', views.MatchCreate.as_view(), name='match-create'),
 
     re_path(r'^matches/(?P<match_pk>[0-9]+)/games/$', views.GameList.as_view(), name='game-list'),
     re_path(r'^matches/(?P<match_pk>[0-9]+)/games/(?P<game_pk>[0-9]+)/$', views.GameDetail.as_view(), name='game-detail'),
