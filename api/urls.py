b7ce878c6302336f8654aef38101f4622220e754
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-07 16:49:53 -0400

    Change . to \w in URL regex, fix lowercase z

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     re_path(r'^players/$', views.PlayerList.as_view(), name='player-list'),
-    re_path(r'^players/(?P<username>[a-zA-z]+.*)/$', views.PlayerDetail.as_view(), name='player-detail'),
+    re_path(r'^players/(?P<username>[a-zA-Z]+\w*)/$', views.PlayerDetail.as_view(), name='player-detail'),
     re_path(r'^players/create/$', views.PlayerCreate.as_view(), name='player-create'),
     re_path(r'^logged-in-player/$', views.LoggedInPlayerDetail.as_view(), name='logged-in-player'),
 
-    re_path(r'^matches/(?P<username>[a-zA-z]+.*)/$', views.MatchList.as_view(), name='match-list'),
+    re_path(r'^matches/(?P<username>[a-zA-Z]+\w*)/$', views.MatchList.as_view(), name='match-list'),
     re_path(r'^matches/(?P<match_pk>[0-9]+)/$', views.MatchDetail.as_view(), name='match-detail'),
     re_path(r'^matches/create/$', views.MatchCreate.as_view(), name='match-create'),
 
@@ -16,11 +16,11 @@ urlpatterns = [
     re_path(r'^matches/(?P<match_pk>[0-9]+)/games/(?P<game_pk>[0-9]+)/$', views.GameDetail.as_view(), name='game-detail'),
     re_path(r'^matches/(?P<match_pk>[0-9]+)/games/create/$', views.GameCreate.as_view(), name='game-create'),
 
-    re_path(r'^players/(?P<username>[a-zA-z]+.*)/scores/$', views.ScoreList.as_view(), name='score-list'),
-    re_path(r'^matches/(?P<match_pk>[0-9]+)/players/(?P<username>[a-zA-z]+.*)/scores/$', views.ScoreDetail.as_view(), name='score-detail'),
+    re_path(r'^players/(?P<username>[a-zA-Z]+\w*)/scores/$', views.ScoreList.as_view(), name='score-list'),
+    re_path(r'^matches/(?P<match_pk>[0-9]+)/players/(?P<username>[a-zA-Z]+\w*)/scores/$', views.ScoreDetail.as_view(), name='score-detail'),
 
-    re_path(r'^players/(?P<username>[a-zA-z]+.*)/outcomes/$', views.OutcomeList.as_view(), name='outcome-list'),
-    re_path(r'^matches/(?P<match_pk>[0-9]+)/players/(?P<username>[a-zA-z]+.*)/outcomes/$', views.OutcomeDetail.as_view(), name='outcome-detail')
+    re_path(r'^players/(?P<username>[a-zA-Z]+\w*)/outcomes/$', views.OutcomeList.as_view(), name='outcome-list'),
+    re_path(r'^matches/(?P<match_pk>[0-9]+)/players/(?P<username>[a-zA-Z]+\w*)/outcomes/$', views.OutcomeDetail.as_view(), name='outcome-detail')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- frontend/urls.py
+++ frontend/urls.py
@@ -2,10 +2,10 @@ from django.urls import include, path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^players/(?P<username>[a-zA-z]+.*)/$', views.player_detail, name='player-detail'),
-    re_path(r'^players/(?P<username>[a-zA-z]+.*)/edit-profile/$', views.player_edit, name='player-edit'),
+    re_path(r'^players/(?P<username>[a-zA-Z]+\w*)/$', views.player_detail, name='player-detail'),
+    re_path(r'^players/(?P<username>[a-zA-Z]+\w*)/edit-profile/$', views.player_edit, name='player-edit'),
 
-    re_path(r'^players/(?P<username>[a-zA-Z]+.*)/matches/$', views.match_list, name='match-list'),
+    re_path(r'^players/(?P<username>[a-zA-Z]+\w*)/matches/$', views.match_list, name='match-list'),
     re_path(r'^matches/(?P<match_pk>[0-9]+)/$', views.match_detail, name='match-detail'),
     re_path(r'^matches/(?P<match_pk>[0-9]+)/edit/$', views.match_edit, name='match-edit'),
 
