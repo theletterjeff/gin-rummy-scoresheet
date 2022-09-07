90cb5e70f8ad8b09315491aa1975edcdd0e08952
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-07 09:58:57 -0400

    Fix player-profile (post-API rebuild)

--- frontend/static/frontend/js/player/player-profile.js
+++ frontend/static/frontend/js/player/player-profile.js
@@ -1,25 +1,51 @@
 import { fillTitle, getJsonResponse } from '../utils.js';
 import { getPlayerFullName } from './utils.js';
 
-let playerJson = await getJsonResponse(
-  window.location.origin + '/api/logged-in-player/')
 fillPlayerPage();
 
-function fillPlayerPage() {
-  fillPlayerPageTitle('');
-  fillProfileCard();
-}
+/**
+ * Main function for filling the player detail page.
+ */
+async function fillPlayerPage() {
+  const username = getUsername();
+  const playerDetailEndpoint = getPlayerDetailEndpoint(username);
+  let playerJson = await getJsonResponse(playerDetailEndpoint);
 
-function fillPlayerPageTitle() {
-  const playerPageTitle = playerJson.username + "'s Profile"
-  fillTitle(playerPageTitle);
+  fillPlayerDetailTitle(username);
+  fillProfileCard(playerJson);
 }
-
-function fillProfileCard() {
+/**
+ * Get the username from the player detail URL.
+ * 
+ * URLs for the `player-detail` page should be formatted as
+ * '^players/(?P<username>[a-zA-z]+.*)/$'
+ */
+ function getUsername() {
+  let urlPath = window.location.pathname;
+  return urlPath.split('/')[2];
+}
+/**
+ * Given a player's username, return the API endpoint for that player's
+ * player-detail view.
+ */
+ function getPlayerDetailEndpoint(username) {
+  return window.location.origin + `/api/players/${username}/`
+}
+/**
+ * Fill the page title for the player detail page.
+ */
+ function fillPlayerDetailTitle(username) {
+  fillTitle(`Gin Rummy Scoresheet - ${username}`);
+}
+/**
+ * Fills data within the player profile card--full name, email, date joined,
+ * last login.
+ */
+function fillProfileCard(playerJson) {
   fillProfileElem('player-username', playerJson.username);
   fillProfileElem('player-name', getPlayerFullName(playerJson));
   fillProfileElem('player-email', playerJson.email);
-  linkPlayerEmail();
+  linkPlayerEmail('player-email', playerJson.email);
   fillProfileElem('player-date-joined', makeDateString(playerJson.date_joined));
   fillProfileElem('player-last-login', makeDateString(playerJson.last_login));
   addEditButtonRedirect();
@@ -38,11 +64,13 @@ function makeDateString(date) {
   return new Date(date).toDateString();
 }
 
-function linkPlayerEmail() {
-  let emailElem = document.getElementById('player-email');
-  emailElem.href = "mailto:" + playerJson.email;
+function linkPlayerEmail(emailElemName, emailValue) {
+  let emailElem = document.getElementById(emailElemName);
+  emailElem.href = "mailto:" + emailValue;
 }
-
+/**
+ * Add link to the "Edit" button that redirects to the player-edit page.
+ */
 function addEditButtonRedirect() {
   const editPlayerEndpoint = window.location.href + 'edit/'
   const editBtn = document.getElementById('edit-button');
