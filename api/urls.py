50d869f83f7785849bc6eeba514bf196cd73874f
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-07 09:47:35 -0400

    Change URLs to raw strings, add 'players' to frontend
    Regex str pattern at front was diverting 'api' endpoints

--- api/urls.py
+++ api/urls.py
@@ -3,24 +3,24 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('players/', views.PlayerList.as_view(), name='player-list'),
-    re_path('players/(?P<username>[a-zA-z]+.*)/$', views.PlayerDetail.as_view(), name='player-detail'),
-    path('players/create/', views.PlayerCreate.as_view(), name='player-create'),
-    path('logged-in-player/', views.LoggedInPlayerDetail.as_view(), name='logged-in-player'),
+    re_path(r'^players/', views.PlayerList.as_view(), name='player-list'),
+    re_path(r'^players/(?P<username>[a-zA-z]+.*)/$', views.PlayerDetail.as_view(), name='player-detail'),
+    re_path(r'^players/create/', views.PlayerCreate.as_view(), name='player-create'),
+    re_path(r'^logged-in-player/', views.LoggedInPlayerDetail.as_view(), name='logged-in-player'),
 
-    re_path('matches/(?P<username>[a-zA-z]+.*)/$', views.MatchList.as_view(), name='match-list'),
-    re_path('matches/(?P<match_pk>[0-9]+)/$', views.MatchDetail.as_view(), name='match-detail'),
-    path('matches/create/', views.MatchCreate.as_view(), name='match-create'),
+    re_path(r'^matches/(?P<username>[a-zA-z]+.*)/$', views.MatchList.as_view(), name='match-list'),
+    re_path(r'^matches/(?P<match_pk>[0-9]+)/$', views.MatchDetail.as_view(), name='match-detail'),
+    re_path(r'^matches/create/', views.MatchCreate.as_view(), name='match-create'),
 
-    re_path('matches/(?P<match_pk>[0-9]+)/games/$', views.GameList.as_view(), name='game-list'),
-    re_path('matches/(?P<match_pk>[0-9]+)/games/(?P<game_pk>[0-9]+)/$', views.GameDetail.as_view(), name='game-detail'),
-    re_path('matches/(?P<match_pk>[0-9]+)/games/create/$', views.GameCreate.as_view(), name='game-create'),
+    re_path(r'^matches/(?P<match_pk>[0-9]+)/games/$', views.GameList.as_view(), name='game-list'),
+    re_path(r'^matches/(?P<match_pk>[0-9]+)/games/(?P<game_pk>[0-9]+)/$', views.GameDetail.as_view(), name='game-detail'),
+    re_path(r'^matches/(?P<match_pk>[0-9]+)/games/create/$', views.GameCreate.as_view(), name='game-create'),
 
-    re_path('players/(?P<username>[a-zA-z]+.*)/scores/$', views.ScoreList.as_view(), name='score-list'),
-    re_path('matches/(?P<match_pk>[0-9]+)/players/(?P<username>[a-zA-z]+.*)/scores/$', views.ScoreDetail.as_view(), name='score-detail'),
+    re_path(r'^players/(?P<username>[a-zA-z]+.*)/scores/$', views.ScoreList.as_view(), name='score-list'),
+    re_path(r'^matches/(?P<match_pk>[0-9]+)/players/(?P<username>[a-zA-z]+.*)/scores/$', views.ScoreDetail.as_view(), name='score-detail'),
 
-    re_path('players/(?P<username>[a-zA-z]+.*)/outcomes/$', views.OutcomeList.as_view(), name='outcome-list'),
-    re_path('matches/(?P<match_pk>[0-9]+)/players/(?P<username>[a-zA-z]+.*)/outcomes/$', views.OutcomeDetail.as_view(), name='outcome-detail')
+    re_path(r'^players/(?P<username>[a-zA-z]+.*)/outcomes/$', views.OutcomeList.as_view(), name='outcome-list'),
+    re_path(r'^matches/(?P<match_pk>[0-9]+)/players/(?P<username>[a-zA-z]+.*)/outcomes/$', views.OutcomeDetail.as_view(), name='outcome-detail')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- frontend/urls.py
+++ frontend/urls.py
@@ -2,16 +2,13 @@ from django.urls import include, path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    re_path(r'^players/(?P<username>[a-zA-z]+.*)/$', views.player_detail, name='player-detail'),
+    re_path(r'^players/(?P<username>[a-zA-z]+.*)/edit-profile/$', views.player_edit, name='player-edit'),
 
-    path('players/', views.player_list, name='player-list'),
-    re_path('players/(?P<username>[a-zA-z]+.*)/$', views.player_detail, name='player-detail'),
-    re_path('players/(?P<username>[a-zA-z]+.*)/edit/$', views.player_edit, name='player-edit'),
+    re_path(r'^players/(?P<username>[a-zA-Z]+.*)/matches/$', views.match_list, name='match-list'),
+    re_path(r'^matches/(?P<match_pk>[0-9]+)/$', views.match_detail, name='match-detail'),
+    re_path(r'^matches/(?P<match_pk>[0-9]+)/edit/$', views.match_edit, name='match-edit'),
 
-    re_path('matches/(?P<username>[a-zA-z]+.*)/$', views.match_list, name='match-list'),
-    re_path('matches/(?P<match_pk>[0-9]+)/$', views.match_detail, name='match-detail'),
-    re_path('matches/(?P<match_pk>[0-9]+)/edit/$', views.match_edit, name='match-edit'),
-
-    re_path('matches/(?P<match_pk>[0-9]+)/games/(?P<game_pk>[0-9]+)/$', views.game_detail, name='game-detail'),
-    re_path('matches/(?P<match_pk>[0-9]+)/games/(?P<game_pk>[0-9]+)/edit/$', views.game_edit, name='game-edit'),
+    re_path(r'^matches/(?P<match_pk>[0-9]+)/games/(?P<game_pk>[0-9]+)/$', views.game_detail, name='game-detail'),
+    re_path(r'^matches/(?P<match_pk>[0-9]+)/games/(?P<game_pk>[0-9]+)/edit/$', views.game_edit, name='game-edit'),
 ]
