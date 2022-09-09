41be9e0bbe1c882fc7e1274ad4c8a098bd4da18f
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-09 14:38:05 -0400

    Register base models in admin

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from base.models import Match, Game, Score, Outcome
+
+admin.site.register(Match)
+admin.site.register(Game)
+admin.site.register(Score)
+admin.site.register(Outcome)
