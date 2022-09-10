452780734f860ed04b525c27a747c5e535861aa1
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-10 15:26:31 -0400

    Rebuild match-detail page

--- frontend/static/frontend/js/match-detail.js
+++ frontend/static/frontend/js/match-detail.js
@@ -6,9 +6,16 @@ import {
   setFormElemAsDisabled,
   getValFromUrl,
 } from "./utils.js";
-import { getApiEndpointFromUrl, getFrontendURL } from './endpoints.js';
+import { getMatchDetailEndpoint, 
+         getPlayerListMatchEndpoint,
+         getGameListMatchEndpoint,
+         getScoreListMatchEndpoint,
+         getOutcomeListMatchEndpoint,
+         getFrontendURL,
+} from './endpoints.js';
 import { fillWinnerDropdown, submitGameForm } from "./game-form.js";
 
+const csrfToken = getCookie('csrftoken');
 fillMatchDetailPage();
 
 /**
@@ -16,57 +23,106 @@ fillMatchDetailPage();
  */
 async function fillMatchDetailPage() {
   // Page constants
-  const matchDetailEndpoint = getApiEndpointFromUrl();
-  const gameListDetailEndpoint = matchDetailEndpoint + 'games/'
-
-  // Data
-  let matchJson = await getJsonResponse(matchDetailEndpoint);
-  let gamesJson = await getJsonResponse(gameListDetailEndpoint);
-  let playersJson = await getJsonResponseArray(matchJson.players);
-  let scoresJson = await getJsonResponseArray(matchJson.score_set);
-
-  fillTitle(`Match - ${playersJson[0].username} v. ${playersJson[1].username}`);
+  const matchPk = getValFromUrl(window.location.pathname, 'matches');
+  const endpoints = getEndpoints(matchPk);
+  let data = await getDataObj(endpoints);
   
-  fillWinnerDropdown(playersJson[0].username, playersJson[1].username);
-  listGames();
-  fillScoreboard(playersJson, gamesJson);
-  
-  let matchOutcome = checkMatchOutcome();
-  if (matchOutcome) {
-    setMatchAsComplete();
-  }
+  fillMatchDetailPageTitle(data.playerList);
+  fillWinnerDropdown(
+    data.playerList.results[0].username,
+    data.playerList.results[1].username,
+  );
+  listGames(data.gameList.results, data.matchDetail);
+  fillScoreboard(
+    data.playerList.results,
+    data.gameList.results,
+    data.scoreList.results,
+  );
 
+  // If match is complete, gray out the game submit form
+  let matchIsComplete = checkMatchOutcome(data.outcomeList);
+  if (matchIsComplete) {
+    setMatchAsComplete(data.outcomeList.results, data.playerList.results);
+  };
+  addGameFormSubmitEvent(matchPk);
+
+  
+}
+async function addGameFormSubmitEvent(matchPk) {
   let newGameForm = document.getElementById("new-game-form");
-  newGameForm.addEventListener("submit", function(e) {
-    submitGameForm(e, 'POST')
-    .then(() => document.getElementById('new-game-form').reset())
-    .then(() => updateMatchJsons())
-    .then(() => listGames())
-    .then(function() {
-      let gamesJson = await getJsonResponse(gameListDetailEndpoint);
-      let playersJson = await getJsonResponseArray(matchJson.players);
-      fillScoreboard(playersJson, gamesJson))
+  newGameForm.addEventListener("submit", async function(e) {
+    await submitGameForm(e, 'POST', matchPk);
+    document.getElementById('new-game-form').reset();
+    fillMatchDetailPage();
   });
 }
-
+/**
+ * Return an object containing the API endpoints for match-detail, 
+ * game-list-match, player-list-match, and score-list-match.
+ */
+function getEndpoints(matchPk) {
+  return [
+    getMatchDetailEndpoint(matchPk),
+    getGameListMatchEndpoint(matchPk),
+    getPlayerListMatchEndpoint(matchPk),
+    getScoreListMatchEndpoint(matchPk),
+    getOutcomeListMatchEndpoint(matchPk),
+  ];
+}
+/**
+ * Return Promise object for the match, match's games, match's players,
+ * and match's scores. `endpoints` is an object of key-value pairs: key is 
+ * name of endpoint, value is endpoint URL.
+ */
+async function getDataJsons(endpoints) {
+  let promises = []
+  for (let endpoint of endpoints) {
+    promises.push(getJsonResponse(endpoint));
+  };
+  return Promise.all(promises);
+}
+/**
+ * Loop through JSONs produced by getDataJsons, assigning them as values 
+ * to an object whose keys are the JSON types (matchDetail, gameList, 
+ * playerList, scoreList).
+ */
+async function getDataObj(endpoints) {
+  let dataVals = await getDataJsons(endpoints);
+  let dataKeys = [
+    'matchDetail',
+    'gameList',
+    'playerList',
+    'scoreList',
+    'outcomeList',
+  ];
+  let dataObj = {};
+  for (let i = 0; i < dataVals.length; i++) {
+    dataObj[dataKeys[i]] = dataVals[i];
+  };
+  return dataObj;
+}
+/**
+ * Fill the match-detail page title.
+ */
+function fillMatchDetailPageTitle(playerListData) {
+  let usernames = [];
+  for (let data of playerListData.results) {
+    usernames.push(data.username);
+  }
+  fillTitle(`Match - ${usernames[0]} v. ${usernames[1]}`);
+}
 /** Fill the `game-wrapper` element with a list of game details */
-async function listGames() {
-  let gamesHTML = await getGamesHTML();
-
+async function listGames(gamesJson, matchJson) {
+  let gamesHTML = await getGamesHTML(gamesJson);
+  
   let gameWrapper = document.getElementById("game-table-body");
   gameWrapper.innerHTML = "";
   gamesHTML.forEach(addGameRowToPage);
-  addEditDeleteButtons(matchJson);
+  addEditDeleteButtons(gamesJson);
 }
-
-async function updateMatchJsons() {
-  matchJson = await getJsonResponse(matchDetailEndpoint);
-  gamesJson = await getJsonResponseArray(matchJson.games);
-  playersJson = await getJsonResponseArray(matchJson.players);
-  scoresJson = await getJsonResponseArray(matchJson.score_set);
-}
-
-/* Return an array of game table row HTML elements */
+/**
+ * Return an array of game table row HTML elements
+ */
 async function getGamesHTML(gamesJson) {
   let gamesHTML = [];
   for (let gameJson of gamesJson) {
@@ -110,7 +166,7 @@ function addGameRowToPage(gameHTML) {
   gameWrapper.innerHTML += gameHTML;
 }
 
-async function addEditDeleteButtons() {
+async function addEditDeleteButtons(gamesJson) {
   let editBtns = document.getElementsByClassName('edit')
   let deleteBtns = document.getElementsByClassName('delete')
 
@@ -126,26 +182,24 @@ async function addEditDeleteButtons() {
 
 function addEditButton(editButtonElem, gameEndpoint) {
   editButtonElem.addEventListener('click', function() {
-    window.location = getFrontendURL(gameEndpoint)
+    window.location = getFrontendURL(gameEndpoint) + 'edit/';
   });
 }
 
 async function addDeleteButton(deleteButtonElem, gameEndpoint) {
-  deleteButtonElem.addEventListener('click', function() {
-    fetch(gameEndpoint, {
+  deleteButtonElem.addEventListener('click', async function() {
+    await fetch(gameEndpoint, {
       method: 'DELETE',
       headers: {
         'Content-type': 'application/json',
         'X-CSRFToken': csrfToken,
       }
     })
-    .then(() => updateMatchJsons())
-    .then(() => listGames())
-    .then(() => fillScoreboard())
+    fillMatchDetailPage();
   })
 }
 
-function fillScoreboard(playersJson, gamesJson) {
+function fillScoreboard(playersJson, gamesJson, scoresJson) {
   let scoreboardUsernameElems = document.getElementsByClassName('scoreboard-username');
   let scoreboardPointsElems = document.getElementsByClassName('scoreboard-points');
   let scoreboardWinsLossesElems = document.getElementsByClassName('scoreboard-wins-losses')
@@ -154,7 +208,7 @@ function fillScoreboard(playersJson, gamesJson) {
 
   for (let i in playersJson) {
     
-    let scoreJson = getScoreJson(playersJson[i])
+    let scoreJson = getScoreJson(playersJson[i], scoresJson)
     
     // Stats
     let points = scoreJson.player_score;
@@ -171,7 +225,7 @@ function fillScoreboardUsernames(scoreboardUsernameElem, playerJson) {
   scoreboardUsernameElem.innerHTML = playerJson.username;
 }
 
-function getScoreJson(playerJson) {
+function getScoreJson(playerJson, scoresJson) {
   return scoresJson.filter(function(score) {
     return score.player == playerJson.url;
   })[0]
@@ -182,7 +236,21 @@ function fillScoreboardPoints(scoreboardPointsElem, points) {
 }
 
 function fillScoreboardWinsLosses(scoreboardWinsLossesElem, wins, losses) {
-  scoreboardWinsLossesElem.innerHTML = `(${wins} Wins, ${losses} losses)`
+  let winWord = null;
+  let lossWord = null;
+
+  if (wins == 1) {
+    winWord = 'Win';
+  } else {
+    winWord = 'Wins';
+  };
+  if (losses == 1) {
+    lossWord = 'Loss';
+  } else {
+    lossWord = 'Losses';
+  };
+
+  scoreboardWinsLossesElem.innerHTML = `(${wins} ${winWord}, ${losses} ${lossWord})`
 }
 
 function countWinsAndLosses(playersJson, gamesJson) {
@@ -202,8 +270,8 @@ function countWinsAndLosses(playersJson, gamesJson) {
   }
 }
 
-function checkMatchOutcome() {
-  return matchJson.outcome_set.length > 0;
+function checkMatchOutcome(outcomeList) {
+  return outcomeList.count > 0;
 }
 
 function disableNewGameFormFields() {
@@ -219,33 +287,31 @@ function disableNewGameFormFields() {
   }
 }
 
-function setMatchAsComplete() {
+function setMatchAsComplete(outcomeList, playerList) {
   disableNewGameFormFields();
-  addWinnerToScoreboardCardTitle();
+  addWinnerToScoreboardCardTitle(outcomeList, playerList);
 }
 
-function getMatchWinnerEndpoint() {
-  let outcomeArray = matchJson.outcome_set;
+function getMatchWinnerEndpoint(outcomeList) {
   let winnerEndpoint = null
-  for (outcomeEndpoint of outcomeArray) {
-    let outcomeJson = getJsonResponse(outcomeEndpoint);
-    if (outcomeJson.player_outcome == 1) {
-      winnerEndpoint = outcomeJson.player;
+  for (let outcome of outcomeList) {
+    if (outcome.player_outcome == 1) {
+      winnerEndpoint = outcome.player;
       break;
     }
   }
   return winnerEndpoint;
 }
 
-function getMatchWinnerUsername() {
-  let winnerEndpoint = getMatchWinnerEndpoint();
-  let winnerJson = playerJson.filter(function(playerJson) {
+function getMatchWinnerUsername(outcomeList, playerList) {
+  let winnerEndpoint = getMatchWinnerEndpoint(outcomeList);
+  let winnerJson = playerList.filter(function(playerJson) {
     return playerJson.url == winnerEndpoint;
   })[0];
   return winnerJson.username;
 }
 
-function addWinnerToScoreboardCardTitle() {
-  let winnerUsername = getMatchWinnerUsername();
+function addWinnerToScoreboardCardTitle(outcomeList, playerList) {
+  let winnerUsername = getMatchWinnerUsername(outcomeList, playerList);
   document.getElementById('scoreboard-card-title').innerHTML = `Scoreboard :: <span><b>${winnerUsername} Wins!</b></span>`
 }
