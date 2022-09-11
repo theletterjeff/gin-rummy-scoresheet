d2f0d930dde04f274d133167a187bd54d9d5d546
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-11 13:52:59 -0400

    Add failing test for match edit button redirect

--- tests/test_frontend/test_selenium_pytest.py
+++ tests/test_frontend/test_selenium_pytest.py
@@ -7,7 +7,6 @@ from accounts.models import Player
 from base.models import Game, Match
 from tests.fixtures import *
 
-
 def test_current_matches_table_contains_incomplete_matches(
         player0, incomplete_match_with_one_game, driver, live_server, wait):
     url = live_server.url + reverse('frontend:match-list', kwargs={'username': 'player0'})
@@ -31,3 +30,18 @@ def test_past_matches_table_contains_finished_complete_matches(
     match_row = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
 
     assert match_row
+
+def test_edit_buttons_on_match_list_page_redirect(
+        driver, player0, incomplete_match_with_one_game,
+        complete_match_with_one_game, live_server, wait):
+    """The edit button on match rows on the match list page redirects to 
+    the detail view for their respective matches.
+    """
+    url = live_server.url + reverse('frontend:match-list', kwargs={'username': 'player0'})
+    driver.get(url)
+
+    for match in [incomplete_match_with_one_game, complete_match_with_one_game]:
+        edit_button = wait.until(EC.presence_of_element_located(
+            (By.ID, f'edit-match-{match.pk}')))
+        edit_button.click()
+        wait.until(EC.url_changes(url))
