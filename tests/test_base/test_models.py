d44d089b8a7ff3831c58e880a21107d73170fcc1
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-17 08:56:16 -0400

    Replace Match model django tests with pytest

--- tests/test_base/test_models.py
+++ tests/test_base/test_models.py
@@ -8,44 +8,27 @@ from django.test import TestCase
 
 from accounts.models import Player
 from base.models import Match, Score
+from tests.fixtures import *
 
-class TestMatchModel(TestCase):
+def test_default_match_has_target_score_500_and_outcome_false(simple_match):
+    """Match instances that are created without any additional parameters
+    have .target_score=500 and .complete=False..
     """
-    Tests for the Match model.
+    assert simple_match.target_score == 500
+    assert simple_match.complete == False
+
+def test_create_match_sets_datetime_started_to_now(mock_now, simple_match):
+    """Creating a new Match sets the `datetime_started` attribute to now."""
+    target_datetime = timezone.datetime(2022, 1, 1, 0, 0, tzinfo=timezone.utc)
+    assert simple_match.datetime_started == target_datetime
+
+def test_match_str(mock_now, simple_match):
+    """String representation of a Match is '<date started string> (<pk>)',
+    e.g., '01/01/22 (3)'.
     """
-    fixtures = ['accounts', 'base']
+    target_str = '01/01/22 (1)'
+    assert simple_match.__str__() == target_str
 
-    def test_default_match_has_target_score_500_and_outcome_false(self):
-        """
-        Match instances that are created without any additional parameters
-        have .target_score=500 and .complete=False..
-        """
-        match = Match()
-        self.assertEqual(match.target_score, 500)
-        self.assertFalse(match.complete)
-    
-    @mock.patch('django.utils.timezone.now')
-    def test_create_match_sets_datetime_started_to_now(self, mock_datetime):
-        """
-        Creating a new Match sets the `datetime_started` attribute to now.
-        """
-        tz = timezone(timedelta(hours=-4))
-        mock_datetime.return_value = datetime(2022, 1, 1, tzinfo=tz)
-        
-        match = Match.objects.create()
-        assert match.datetime_started == mock_datetime()
-
-    def test_str(self):
-        """
-        String representation of a Match is '<date started string> (<pk>)',
-        e.g., '01/01/22 (3)'.
-        """
-        # Load match from fixture
-        match = Match.objects.get(pk=1)
-
-        target_str = '04/02/22 (1)'
-
-        self.assertEqual(match.__str__(), target_str)
 
 class TestScoreModel(TestCase):
     """
