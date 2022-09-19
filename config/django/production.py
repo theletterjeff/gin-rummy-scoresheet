8156a14a2eae244168b1d17ab14f0ea04ed7459a
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-19 10:27:57 -0400

    Remove duplicate staticfiles app

--- config/django/production.py
+++ config/django/production.py
@@ -4,7 +4,6 @@ from config.django.base import *
 
 ALLOWED_HOSTS = ['gin-rummy-scoresheet.herokuapp.com']
 DEBUG = False
-INSTALLED_APPS += ["django.contrib.staticfiles"]
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
