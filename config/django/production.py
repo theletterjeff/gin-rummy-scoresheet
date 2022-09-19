c849834cd8cd95cc4b09fa88a539b67ee09aa4b2
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-19 09:53:52 -0400

    Fix allowed hosts in production (add hyphens)

--- config/django/production.py
+++ config/django/production.py
@@ -1,5 +1,5 @@
 from config.django.base import *
 
-ALLOWED_HOSTS = ['ginrummyscoresheet.herokuapp.com']
+ALLOWED_HOSTS = ['gin-rummy-scoresheet.herokuapp.com']
 DEBUG = False
 INSTALLED_APPS += ["django.contrib.staticfiles"]
